ff0ac1e1477812dd7b8cd47754fe285b704472a5
oyesa <mercyoyesa2018@gmail.com>
2022-06-03 11:35:03 +0300

    create image model

--- instagram/models.py
+++ instagram/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+
+
 
 # Create your models here.
+class Image(models.Model):
+  user=models.ForeignKey(User, on_delete=models.CASCADE, related_name='images')
+  image=CloudinaryField('image')
+  image_name=models.CharField(max_length=200)
+  image_description=models.TextField()
+  image_date=models.DateTimeField(auto_now_add=True)
+  profile=models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+  def __str__(self):
+    return self.image_name

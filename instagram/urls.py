81bc10b469ef81a7dc0de4f1207bce75e7a4fd70
oyesa <mercyoyesa2018@gmail.com>
2022-06-03 21:02:38 +0300

    create url paths for all view functions

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,7 +1,21 @@
-from django.urls import path, re_path
 from . import views
+from django.urls import path, re_path
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 
 urlpatterns =[
   path('', views.home, name='home'),
+  path('profile/', views.profile, name='profile'),
+  path('profile/update/', views.profile, name='profile'),
+  path('user/<int:id>/', views.user_profile, name='user.profile'),
+  path('like/<int:id>/', views.image_likes, name='image.likes'),
+  path('comment/add', views.save_comment, name='comment.add'),
+  path('upload/add/', views.save_image, name='save.image'),
+  path('search/', views.search_images, name='search.images'),
+  path('images/<int:id>/', views.single_image, name='single.image')
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)

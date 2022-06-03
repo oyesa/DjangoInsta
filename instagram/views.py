d7953e3e6cedf9f9e582f1cc2b200a8fcd027f23
oyesa <mercyoyesa2018@gmail.com>
2022-06-03 08:14:53 +0300

    create profile view function

--- instagram/views.py
+++ instagram/views.py
@@ -1,8 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http  import HttpResponse
 
 
 
 # Create your views here.
+
 def home(request):
   return render(request, 'home.html')
+
+
+#profile view
+def profile(request):
+  return render(request, 'profile.html')
+
+def update_profile(request):
+  return render(request, 'profile.html')

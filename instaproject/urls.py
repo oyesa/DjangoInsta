c4033aae608009fb2c6f53050efdce3f057066c4
oyesa <mercyoyesa2018@gmail.com>
2022-06-04 10:39:33 +0300

    add home template details

--- instaproject/urls.py
+++ instaproject/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django_registration.backends.one_step.views import RegistrationView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('instagram.urls'))
+    path('', include('instagram.urls')),
+    path('accounts/register/', RegistrationView.as_view(success_url='/'), name='django_registration_register'),
+    path('accounts/', include('django_registration.backends.one_step.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]

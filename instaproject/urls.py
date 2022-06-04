8dde1d12e759ca222c7cd648842f50cdf43d09a1
oyesa <mercyoyesa2018@gmail.com>
2022-06-04 19:41:32 +0300

    styling app

--- instaproject/urls.py
+++ instaproject/urls.py
@@ -15,12 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django_registration.backends.one_step.views import RegistrationView
+# from django_registration.backends.one_step.views import RegistrationView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('instagram.urls')),
-    path('accounts/register/', RegistrationView.as_view(success_url='/'), name='django_registration_register'),
-    path('accounts/', include('django_registration.backends.one_step.urls')),
+    path('accounts/', include('django_registration.backends.activation.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    # path('accounts/register/', RegistrationView.as_view(success_url='/'), name='django_registration_register'),
+    # path('accounts/', include('django_registration.backends.one_step.urls')),
+    # path('accounts/', include('django.contrib.auth.urls')),
 ]

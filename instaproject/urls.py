483cf4e8541404643346b737507997869649e912
oyesa <mercyoyesa2018@gmail.com>
2022-06-05 14:03:34 +0300

    change homepage template and design

--- instaproject/urls.py
+++ instaproject/urls.py
@@ -14,14 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 # from django_registration.backends.one_step.views import RegistrationView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('instagram.urls')),
-    path('accounts/', include('django_registration.backends.activation.urls')),
+    path('accounts/', include('django_registration.backends.one_step.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    re_path(r'^tinymce/', include('tinymce.urls'))
     # path('accounts/register/', RegistrationView.as_view(success_url='/'), name='django_registration_register'),
     # path('accounts/', include('django_registration.backends.one_step.urls')),
     # path('accounts/', include('django.contrib.auth.urls')),

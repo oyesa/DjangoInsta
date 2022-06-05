913460f7fa7e6d5818d678a970acb574a061a1ff
oyesa <mercyoyesa2018@gmail.com>
2022-06-05 10:56:50 +0300

    update navbar design

--- instaproject/settings.py
+++ instaproject/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'instagram.apps.InstagramConfig',
     'bootstrap5',
     'cloudinary',
+    'tinymce',
 ]
 
 
@@ -131,8 +132,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static/') ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
